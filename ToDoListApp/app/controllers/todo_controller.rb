2fc8d43d76a259673f21f6dd976421f1bb270b41
RuxaiNezers <alexis.neira.cruz@gmail.com>
2018-09-06 18:27:57 -0300

    user's history 6, list completed, less profile pic

--- ToDoListApp/app/controllers/todo_controller.rb
+++ ToDoListApp/app/controllers/todo_controller.rb
@@ -15,10 +15,20 @@ class TodoController < ApplicationController
   end
 
   def show
+    @done = []
     @users = []
     @Todo= Todo.find(params[:id])
     if user_signed_in?  
-      @users = User.joins(:completeds).where(completeds: {todo_id: @Todo})
+      @users = User.joins(:completeds).where(completeds: {todo_id: @Todo, done: true})
+       @Done = Completed.select(:user_id, :todo_id).where(:user_id => current_user, :done => true).all
+      @Done.each do |done|
+        @done << done.todo_id 
+      end
+      @completes = @done.length
+      @total = Todo.all.count
+
+
+      @Top5 = User.joins(:completeds).where(completeds: {todo_id: @Todo, done: true}).order('updated_at').limit(5)
     end
   end
 
